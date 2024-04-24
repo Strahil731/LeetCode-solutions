42581ab3a70e73231ee88c4f85bdaa71b3a6bb67
Strahil731 <157738598+Strahil731@users.noreply.github.com>
2024-04-24 10:34:35 +0300

    Update Range Sum of BST.js

--- Range Sum of BST.js
+++ Range Sum of BST.js
@@ -15,3 +15,18 @@ function rangeSumBST(root, low, high) {
 }
 
 rangeSumBST([10, 5, 15, 3, 7, 13, 18, 1, null, 6], 6, 10)
+
+function rangeSumBST1(root, low, high) {
+    if (!root) {
+        return 0;
+    }
+
+    const currentVal = (root.val >= low && root.val <= high) ? root.val : 0;
+
+    const leftSum = rangeSumBST(root.left, low, high);
+    const rightSum = rangeSumBST(root.right, low, high);
+
+    return currentVal + leftSum + rightSum;
+}
+
+rangeSumBST1([10, 5, 15, 3, 7, 13, 18, 1, null, 6], 6, 10)
